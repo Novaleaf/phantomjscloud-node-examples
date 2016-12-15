ad7704c897b9285fbeecc0af755b9536b6cc69ba
Jason <JasonS@Novaleaf.com>
2016-12-15 11:48:53 -0800

    make xlibConfigDefaults extensible by nested modules

--- dist/refs.js
+++ dist/refs.js
@@ -1,4 +1,21 @@
 "use strict";
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+//specify xlib config options, without requiring environmental config
+//if ((global as any)._xlibConfigDefaults == null) { (global as any)._xlibConfigDefaults = {}; }
+global._xlibConfigDefaults = __assign({}, {
+    logLevel: "DEBUG",
+    envLevel: "PREPROD",
+    isTest: "FALSE",
+    isDev: "TRUE",
+    sourceMapSupport: true,
+}, global._xlibConfigDefaults);
 exports.xlib = require("xlib");
 exports.phantomJsCloud = require("phantomjscloud");
 //# sourceMappingURL=refs.js.map
--- src/refs.ts
+++ src/refs.ts
@@ -1,4 +1,16 @@
-﻿export import xlib = require("xlib");
+﻿//specify xlib config options, without requiring environmental config
+//if ((global as any)._xlibConfigDefaults == null) { (global as any)._xlibConfigDefaults = {}; }
+(global as any)._xlibConfigDefaults = {
+    ...{
+        logLevel: "DEBUG",
+        envLevel: "PREPROD",
+        isTest: "FALSE",
+        isDev: "TRUE",
+        sourceMapSupport: true,
+    } as typeof _xlibConfigDefaults,
+    ...(global as any)._xlibConfigDefaults,
+};
+export import xlib = require("xlib");
 
 export import phantomJsCloud = require("phantomjscloud");
 
